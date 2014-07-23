2fce2296f09a8376680ade5ff85d942c65a4541d
jonavargas <jonavargas2012@gmail.com>
2014-07-22 19:33:16 -0600

    cambios

---
+++ RestaurantMenu/RestaurantMenu/Controllers/MenuController.cs
@@ -0,0 +1,34 @@
+﻿using RestaurantMenu.DataAccess;
+using RestaurantMenu.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantMenu.Controllers
+{
+    public class MenuController : Controller
+    {
+        private RestaurantMenuEntities db = new RestaurantMenuEntities();
+        //
+        // GET: /Menu/
+
+        public ActionResult Index(int id = 0)
+        {
+            //model = db.Restaurant.Where(model => model.Id).ToList();
+            Restaurant restaurant = db.Restaurants.Find(id);
+            var menu = new MenuModel()
+            {
+                name = restaurant.Name,
+                category = restaurant.RestaurantCategory.Name,
+                meals = restaurant.Meals.ToList(),
+                telefono = restaurant.Telephone
+            };
+
+            return View(menu);
+        }
+
+
+    }
+}
---
+++ RestaurantMenu/RestaurantMenu/Models/MenuModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantMenu.Models
+{
+    public class MenuModel
+    {
+        public string name { get; set; }
+        public string category { get; set; }
+        public List<RestaurantMenu.DataAccess.Meal> meals { get; set; }
+        public string telefono { get; set; }
+        public List<RestaurantMenu.DataAccess.Meal> menu { get; set; }
+    }
+}
